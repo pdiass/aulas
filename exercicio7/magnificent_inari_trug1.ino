82a0e0598a34167b79eaa518fb4c17ff7c673120
pdiass <99338986+pdiass@users.noreply.github.com>
2022-03-11 13:51:22 -0300

    Add files via upload

---
+++ exercicio7/magnificent_inari_trug1.ino
@@ -0,0 +1,67 @@
+const int botao1 = 3;
+const int botao2 = 4;
+const int botao3 = 5;
+const int botao4 = 6;
+const int botao5 = 7;
+const int botao6 = 8;
+const int botao7 = 9;
+
+int var1;
+int var2;
+int var3;
+int var4;
+int var5;
+int var6;
+int var7;
+
+ void setup()
+ { 
+      pinMode(botao1, INPUT);
+      pinMode(botao2, INPUT);
+      pinMode(botao3, INPUT); 
+      pinMode(botao4, INPUT); 
+      pinMode(botao5, INPUT); 
+      pinMode(botao6, INPUT);
+      pinMode(botao7, INPUT);
+      Serial.begin(9600);
+ }
+ 
+
+
+ void loop()
+ {
+    
+var1 = digitalRead(botao1);
+var2 = digitalRead(botao2);
+var3 = digitalRead(botao3);
+var4 = digitalRead(botao4);
+var5 = digitalRead(botao5);
+var6 = digitalRead(botao6);
+var7 = digitalRead(botao7);
+
+ if (var1){
+   Serial.println("Do");
+ }
+
+ if (var2){
+   Serial.println("Re");
+ }
+
+ if (var3){
+   Serial.println("Mi");
+ }
+
+ if (var4){
+   Serial.println("Fa");
+ }
+
+ if (var5){
+   Serial.println("Sol");
+ }
+ if (var6){
+   Serial.println("La");
+ }
+ if (var7){
+   Serial.println("Si");
+ }
+ }
